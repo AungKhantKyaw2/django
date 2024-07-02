b0f63e9b7e4b5ce420daf50b97b5c5452024b046
AungKhantKyaw2 <japangatone2020@gmail.com>
2024-07-02 16:57:41 +0700

    views.py

--- mysite/quest/views.py
+++ mysite/quest/views.py
@@ -9,7 +9,9 @@ pipe = pipeline("text-generation", model="Aung2024/gpt2-large-span-head-finetune
 def home(request):
     if request.method == 'POST':
         input_text = request.POST.get('input_text')
-        quest_ans = pipe(input_text)
+        context_text = request.POST.get('context_text')
+        full_text =f"Question: {input_text} Context: {context_text}"
+        quest_ans = pipe(full_text)
         print (quest_ans)
-        return render(request,'index.html',{'quest_ans': quest_ans})
+        return render(request,'index.html',{'quest_ans': quest_ans, 'input_text': input_text, 'context_text': context_text})
     return render(request,'index.html')  
